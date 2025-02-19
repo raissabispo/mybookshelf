1b10589ce60ff4a10db14ec8000d100b87408f25
Raissa <uamr.ieadpe1@gmail.com>
2025-02-18 22:59:06 -0300

    Adiconando funções de pesquisar livros

---
+++ js/livros.js
@@ -0,0 +1,39 @@
+
+function pesquisarLivros() {
+    const query = document.getElementById("pesquisar").value.toLowerCase();
+    const mensagemErro = document.getElementById("mensagem-erro");  
+    if (!query) {
+        mensagemErro.innerHTML = `
+            <div class="mensagem-erro">
+                <p>Nada foi encontrado. Precisa digitar o Título do livro.</p>
+            </div>
+        `;
+        return;
+    }
+    fetch('/js/livros.json')
+        .then(response => response.json())
+        .then(data => {
+            const resultados = data.filter(livro => 
+                livro.titulo.toLowerCase().includes(query)
+            );
+            exibirResultados(resultados);
+        });
+
+       
+}
+    
+function exibirResultados(resultados) {
+    const divResultados = document.getElementById("livros");
+    divResultados.innerHTML = '';
+    resultados.forEach(livro => {
+        const livroDiv = document.createElement("div");
+        livroDiv.classList.add("livro");
+        livroDiv.innerHTML = `
+            <h2 class="titulo-livro">${livro.titulo}</h2>
+            <p>${livro.autor}</p>
+            <p>${livro.descricao}</p>
+            <img src="${livro.capa}" alt="Capa do livro ${livro.titulo}">
+        `;
+        divResultados.appendChild(livroDiv);
+    });
+}
--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-let nome = document.querySelector("#nome");
-let autor = document.querySelector("#autor");
-let  genero = document.querySelector("#genero");
-let descricao = document.querySelector("#descricao");
-let fotoLivro = document.querySelector("#foto-livro");
-
 
 // estrelas
 
