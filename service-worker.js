43cf7df6259dffc30d574c59153c43196b3c0aea
Raissa <uamr.ieadpe1@gmail.com>
2025-03-07 19:42:43 -0300

    Adicionando icones, manifest e service-worker

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'mybookshelf-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles/style.css',
+  '/js/livros.js',
+  '/js/livros.json',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Atualizando o cache quando o Service Worker é ativado
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
