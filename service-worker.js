640dca6001de035ed7c8b519c3816f61b03932c5
Raissa <uamr.ieadpe1@gmail.com>
2025-03-09 06:57:02 -0300

    Atualizando o service-worker

--- service-worker.js
+++ service-worker.js
@@ -5,9 +5,11 @@ const urlsToCache = [
   '/styles/style.css',
   '/js/script.js',
   '/js/livros.js',
-  '/manifest.json' 
+  'js/livros.json',  
+  '/manifest.json', 
 ];
 
+// Instalação do Service Worker
 self.addEventListener('install', event => {
   console.log('Service Worker instalado!');
   event.waitUntil(
@@ -16,55 +18,38 @@ self.addEventListener('install', event => {
       return cache.addAll(urlsToCache);
     })
   );
-  self.skipWaiting(); 
 });
 
+// Ativação do Service Worker
 self.addEventListener('activate', event => {
   console.log('Service Worker ativado!');
-  event.waitUntil(
+  event.waitUntil(  
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.filter(cache => cache !== CACHE_NAME)
-          .map(cache => caches.delete(cache)) 
+          .map(cache => caches.delete(cache))  // Limpa caches antigos
       );
-    }).then(() => {
-      
-      self.clients.claim();
     })
   );
 });
 
-
+// Fetch (requisição) para servir os arquivos a partir do cache
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-     
-      if (cachedResponse) {
-        event.waitUntil(
-          fetch(event.request).then(networkResponse => {
-            if (networkResponse && networkResponse.ok) {
-              caches.open(CACHE_NAME).then(cache => {
-                cache.put(event.request, networkResponse.clone()); 
-              });
-            }
-          })
-        );
-        return cachedResponse; 
-      }
+  event.respondWith(  
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(networkResponse => {
+  
+        // Clonando a resposta para atualizar o cache
+        const clonedResponse = networkResponse.clone();
 
-      return fetch(event.request).then(networkResponse => {
         if (networkResponse && networkResponse.ok) {
           caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, networkResponse.clone()); 
+            cache.put(event.request, clonedResponse);  // Atualiza o cache
           });
         }
-        return networkResponse;
+
+        return networkResponse; // Retorna a resposta da rede
       });
     })
   );
-});
-self.addEventListener('message', event => {
-  if (event.data === 'skipWaiting') {
-    self.skipWaiting();
-  }
 });
