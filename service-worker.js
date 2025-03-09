8a1b928b7f7fc6a41f4505c0119fe20e5ded055a
Raissa <uamr.ieadpe1@gmail.com>
2025-03-09 06:26:09 -0300

    Editando o PWA

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,53 @@
-const CACHE_NAME = 'mybookshelf-cache';
+const CACHE_NAME = 'mybookshelf-cache-v2'; 
 const urlsToCache = [
   '/',
   '/index.html',
   '/styles/style.css',
   '/js/script.js',
   '/js/livros.js',
-  '/js/livros.json',
-  '/manifest.json'
+  '/manifest.json' 
 ];
 
+
 self.addEventListener('install', event => {
+  console.log('Service Worker instalado!');
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Arquivos armazenados no cache:', urlsToCache);
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-// Atualizando o cache quando o Service Worker é ativado
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
+  console.log('Service Worker ativado!');
+  event.waitUntil(  
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames.filter(cache => cache !== CACHE_NAME)
+          .map(cache => caches.delete(cache))  
       );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(  
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(networkResponse => {
+  
+        const clonedResponse = networkResponse.clone();
+
+
+        if (networkResponse && networkResponse.ok) {
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, clonedResponse); 
+          });
+        }
+
+        return networkResponse;
+      });
     })
   );
 });
