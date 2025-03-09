ca7f01ca5458ea865d62a88ee35ab172d12a3e4e
Raissa <uamr.ieadpe1@gmail.com>
2025-03-09 06:51:49 -0300

    Atualizando o service-worker

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,6 @@ const urlsToCache = [
   '/manifest.json' 
 ];
 
-
 self.addEventListener('install', event => {
   console.log('Service Worker instalado!');
   event.waitUntil(
@@ -17,37 +16,55 @@ self.addEventListener('install', event => {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting(); 
 });
-
 
 self.addEventListener('activate', event => {
   console.log('Service Worker ativado!');
-  event.waitUntil(  
+  event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.filter(cache => cache !== CACHE_NAME)
-          .map(cache => caches.delete(cache))  
+          .map(cache => caches.delete(cache)) 
       );
+    }).then(() => {
+      
+      self.clients.claim();
     })
   );
 });
 
+
 self.addEventListener('fetch', event => {
-  event.respondWith(  
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(networkResponse => {
-  
-        const clonedResponse = networkResponse.clone();
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+     
+      if (cachedResponse) {
+        event.waitUntil(
+          fetch(event.request).then(networkResponse => {
+            if (networkResponse && networkResponse.ok) {
+              caches.open(CACHE_NAME).then(cache => {
+                cache.put(event.request, networkResponse.clone()); 
+              });
+            }
+          })
+        );
+        return cachedResponse; 
+      }
 
-
+      return fetch(event.request).then(networkResponse => {
         if (networkResponse && networkResponse.ok) {
           caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, clonedResponse); 
+            cache.put(event.request, networkResponse.clone()); 
           });
         }
-
         return networkResponse;
       });
     })
   );
+});
+self.addEventListener('message', event => {
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
